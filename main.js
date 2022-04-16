32942bfecc4a967b651f3bc9a3fffe3a31cfdce4
duykhanh <khanhtd@m2w.vn>
2022-04-16 13:46:15 +0700

    update

--- main.js
+++ main.js
@@ -1,127 +1,93 @@
 'use strict';
 
+import {list, listInput} from './mock.js'
+
 (() => {
-    let items = document.querySelectorAll('.item');
-    let drop = document.querySelector('.drop');
+    const $ = document.querySelector.bind(document);
+    const $$ = document.querySelectorAll.bind(document);
+
+    const mainList = $('#list_file');
+
+    let infoItem = null;
     let isResize = false;
-    /*----------  ----------*/
+    let stateItem = {};//==> Chứa các thông tin các phần tử khi được thêm vào văn bản
 
-    function addItem(e) {
-        const { type } = e.dataset;
-        const { innerText, localName, src } = e;
-        const container = document.createElement('div');
-        const el = document.createElement(localName);
-
-        if (type === 'style_1_item') {
-            el.innerHTML = `<div class="editor" contenteditable="true">${innerText}</div>`;
-        }
-
-        if (type === 'style_2_item') {
-            el.src = src;
-        }
-
-        /********** add style **********/
-        container.classList.add(type, 'item_drop');
-        container.innerHTML = `
-        <div class="resize">
-        </div>
-        <div class="close">
-        X
-        </div>
-        <svg 
-        class="resize-button" 
-        height="10" width="10"></svg>
-        `;
-        el.classList.add(type, 'item_main_drop');
-        container.style.top = 0;
-        container.style.let = 0;
-        container.appendChild(el);
-        drop.appendChild(container);
-    }
-
-    function handler() {
-        items.forEach((el) =>
-            el.addEventListener('click', function () {
-                addItem(el);
-                getItemDrop();
-            })
-        );
-    }
-
+    //**********************
+    //* Hàm này sẽ lấy ra các item đã được thêm vào DOM
+    //* Xử lí di chuyển vị trí, kích thước các item trong vùng chưa văn bản
+    //**********************
     function getItemDrop() {
-        const dropItems = document.querySelectorAll('.item_drop');
+        const dropItems = $$('.item_drop');
+
         dropItems.forEach((el) => {
+            const {page, id} = el.dataset;
+
+            const item = stateItem[`${page}`].filter((item) => item.id === +id);
+            const containerPage = $(`#page${page}`);
+
+            /*---------- Xóa phần tử ----------*/
             el.addEventListener('click', function (e) {
                 if (e.target.matches('.close')) {
-                    drop.removeChild(el);
+                    const newSate = stateItem[`${page}`].filter((item) => item.id !== +id);
+                    updateState(newSate, page);
                 }
             });
+
 
             el.addEventListener('mousedown', function (e) {
                 el.style.zIndex = '10';
 
-                const maxX = drop.offsetWidth;
-                const maxY = drop.offsetHeight;
-
-                let resizeTarget;
+                const maxX = containerPage.offsetWidth;
+                const maxY = containerPage.offsetHeight;
 
                 let left = el.offsetLeft;
                 let top = el.offsetTop;
+
+                /*---------- chiều rộng và chiều cao phần tử ----------*/
+                const offsetHeight  = item[0].height;
+                const offsetWidth  = item[0].width;
+
                 /*---------- Vị trí hiện tại của con trỏ ----------*/
                 let mouseX = e.clientX;
                 let mouseY = e.clientY;
 
                 if (e.target.matches('.resize-button')) {
                     isResize = true;
-                    resizeTarget = e.target;
                 } else {
                     isResize = false;
-                    resizeTarget = null;
                 }
 
                 document.onmousemove = function (e) {
+                    /*---------- Vị trí phần tử ----------*/
+                    const dx = mouseX - e.clientX;
+                    const dy = mouseY - e.clientY;
+                    const px = left - dx;
+                    const py = top - dy;
+
+                    const X = getXY(px, maxX - offsetWidth);
+                    const Y = getXY(py, maxY - offsetHeight);
+
                     if (!isResize) {
-                        /*---------- chiều rộng và chiều cao phần tử ----------*/
-                        const { offsetHeight, offsetWidth } = el;
-
-                        /*---------- Vị trí phần tử ----------*/
-                        const dx = mouseX - e.clientX;
-                        const dy = mouseY - e.clientY;
-                        const px = left - dx;
-                        const py = top - dy;
-
                         /********** Cập nhật style item **********/
-                        /*===========> Trục X <==========*/
-                        if (maxX - offsetWidth >= px && px >= 0) {
-                            /*---------- Trong khoảng cho phép ----------*/
-                            el.style.left = px + 'px';
-                        } else {
-                            if (px >= maxX - offsetWidth) {
-                                /*---------- Khi lớn hơn chiều rộng vùng chứa ----------*/
-                                el.style.left = maxX - offsetWidth + 'px';
-                            } else {
-                                /*---------- Khi nhỏ hơn chiều rộng vùng chứa ----------*/
-                                el.style.left = 0 + 'px';
-                            }
-                        }
+                        el.style.left = X + 'px';
+                        el.style.top = Y + 'px';
 
-                        /*===========> Trục Y <==========*/
-                        if (maxY - offsetHeight >= py && py >= 0) {
-                            /*---------- Trong khoảng cho phép ----------*/
-                            el.style.top = py + 'px';
-                        } else {
-                            if (py >= maxY - offsetHeight) {
-                                /*---------- Khi lớn hơn chiều rộng vùng chứa ----------*/
-                                el.style.top = maxY - offsetHeight + 'px';
-                            } else {
-                                /*---------- Khi nhỏ hơn chiều rộng vùng chứa ----------*/
-                                el.style.top = 0 + 'px';
+                        const newSate = stateItem[`${page}`].map((item) => {
+                            if(item.id === +id) {
+                                return {...item, X, Y};
                             }
-                        }
+                            return item;
+                        });
+                        updateState(newSate, page);
                     } else {
                         el.classList.add('show');
+
+                        const {type} = el.dataset;
                         const startX = e.clientX;
                         const startY = e.clientY;
+
+                        const infoItem = listInput.filter((item)=>item.type === type);
+
                         const startWidth = parseInt(
                             document.defaultView.getComputedStyle(el).width,
                             10
@@ -130,12 +96,26 @@
                             document.defaultView.getComputedStyle(el).height,
                             10
                         );
+
                         document.onmousemove = (e) => {
-                            doDrag(
+                            const {WIDTH, HEIGHT} = doDrag(
                                 e,
                                 { width: startWidth, height: startHeight },
-                                { x: startX, y: startY }
+                                { x: startX, y: startY },
+                                { minWidth: infoItem[0].minWidth, minHeight: infoItem[0].minHeight},
+                                { maxWidth: maxX, maxHeight: maxY},
+                                { left: X, top: Y},
                             );
+
+                            el.style.width = WIDTH + 'px';
+                            el.style.height = HEIGHT + 'px';
+                            const newSate = stateItem[`${page}`].map((item) => {
+                                if(item.id === +id) {
+                                    return {...item, width: WIDTH, height: HEIGHT};
+                                }
+                                return item;
+                            });
+                            updateState(newSate, page);
                         };
                     }
                 };
@@ -149,17 +129,222 @@
                     }
                 });
             });
-
-            function doDrag(e, startSize, start) {
-                el.style.width = startSize.width + e.clientX - start.x + 'px';
-                el.style.height = startSize.height + e.clientY - start.y + 'px';
-            }
         });
     }
 
-    function start() {
+    //**********************
+    //* Hàm này sẽ hiển thị các item bên thanh tab
+    //**********************
+    function renderItem() {
+        const container = $('.main_input')
+        container.innerHTML = listInput.map(item => item.html).join('')
+    };
+
+    //**********************
+    //* Hàm này hiển thị các trang PDF 
+    //**********************
+    function renderListFiles() {
+        mainList.innerHTML = list.map((file, index)=>{
+            return (`
+                <div 
+                    ondragover="return false" 
+                    class="drop"
+                    style="width: ${file.width}px; height: ${file.height}px; position: relative" 
+                >
+                    <img
+                        class="img_bg"
+                        alt="abc"
+                        src="${file.link}"
+                    />
+                    <div 
+                        ondragover="return false" 
+                        id="page${index+1}" 
+                        class="main_drop" 
+                        data-index="${index+1}"
+                        style="width: ${file.width}px; height: ${file.height}px; position: absolute; top: 0; left: 0;" >
+                    </div>
+                </div> 
+            `);
+        }).join('');
+
+    };
+
+    //**********************
+    //* Hàm này sẽ xử lí việc người dùng kéo, thả để thêm item vào vùng chứa văn bản
+    //**********************
+    function handlerDrop() {
+        const listContainer = $$('.main_drop');
+
+        function onDrop(ev, pageCurrent) {
+            const {layerX, layerY} = ev;
+
+            addItem(pageCurrent, layerX, layerY);
+            ev.stopPropagation();
+            return false;
+        }
+
+        listContainer.forEach((el)=> {
+            const pageCurrent = el.dataset.index;
+            el.addEventListener('drop', (ev) => onDrop(ev, pageCurrent))
+        });
+    };
+
+    //**********************
+    //* Hàm này sẽ thêm các phần tử vào văn bản
+    //* index: số trang
+    //* x: tọa độ x của trang, y: tọa độ y của trang (so với document)
+    //**********************
+    function addItem(index, x, y) {
+        const page = $(`#page${index}`);
+
+        const id = new Date().getTime();
+        const {clientHeight, clientWidth} = page;
+        const {type, style, htmlInsert, defaultWidth, defaultHeight} = infoItem;
+
+        const top = y - defaultHeight / 2;
+        const left = x - defaultWidth / 2;
+
+        const X = getXY(left, clientWidth - defaultWidth);
+        const Y = getXY(top, clientHeight - defaultHeight);
+
+        !stateItem[`${index}`] ? stateItem[`${index}`] = []:null;
+        stateItem[`${index}`].push({
+            id,
+            X,
+            Y,
+            type,
+            width: defaultWidth,
+            height: defaultHeight,
+            pageNumber: index,
+            htmlInsert,
+            style,
+        });
+        renderItemOnPage();
+    };
+
+    function renderItemOnPage() {
+        for(let i in stateItem) {
+            const html = stateItem[i].map((item) => {
+                /*---------- Tạo thẻ mới để chứa item ----------*/
+                return (`
+                    <div 
+                        class="${item.style} item_drop" 
+                        data-id="${item.id}" 
+                        data-page="${item.pageNumber}"
+                        data-type="${item.type}"
+                        ondragover="return true"
+                        style="
+                            width: ${item.width}px; 
+                            height: ${item.height}px;
+                            left: ${item.X}px;
+                            top: ${item.Y}px;
+                        "
+                    >
+                        <div class="resize"></div>
+                        <div class="close">X</div>
+                        <svg
+                            class="resize-button"
+                            height="10" 
+                            width="10"
+                        >
+                        </svg>
+                        ${item.htmlInsert}
+                    </div>`);
+            });
+
+            /*---------- Thêm item mới vào DOM ----------*/
+            $(`#page${i}`).innerHTML = html.join('');
+        }
+        /*---------- Cập nhật lại các item ----------*/
         getItemDrop();
-        handler();
     }
+
+    //**********************
+    //* Hàm này sẽ tạo mới phần tử để chuẩn bị thêm vào vùng chưa vản bản
+    //* Ví dụ: Chữ ký, ô check box,....
+    //**********************
+    function handlerCreateItem() {
+        const items = $$('.item');
+
+        function createItem(e) {
+            const { type } = e.dataset;
+            const info = listInput.filter((item) => item.type === type);
+
+            infoItem = info[0];
+        };
+
+        items.forEach((el) =>
+            el.addEventListener('drag', function () {
+                createItem(el);
+            })
+        );
+    };
+
+    setInterval(()=>{
+        console.log(stateItem);
+    }, 2000)
+
+    //**********************
+    //* Site function
+    //**********************
+    function updateState (newSate, page) {
+        stateItem[`${page}`] = newSate;
+        renderItemOnPage();
+    }
+
+    function doDrag(e, startSize, start, minSize, maxSize,coordinates) {
+        
+        const width = startSize.width + e.clientX - start.x;
+        const height = startSize.height + e.clientY - start.y;
+
+        const WIDTH = coordinates.left + width < maxSize.maxWidth ? 
+                        getWidthHeight(minSize.minWidth, width):
+                        width - coordinates.left - (width - maxSize.maxWidth);
+
+        const HEIGHT = coordinates.top + height < maxSize.maxHeight ? 
+                        getWidthHeight(minSize.minHeight, height):
+                        height - coordinates.top - (height - maxSize.maxHeight);
+
+        return {WIDTH, HEIGHT};
+    }
+
+    function getWidthHeight(minSize, currentSize) {
+        if(currentSize <= minSize) {
+            return minSize;
+        } else {
+            return currentSize;
+        };
+    }
+
+    function getXY(coordinates, max){
+        if(coordinates < 0) {
+            return 0
+        } else {
+            if(coordinates >= max) {
+                return max;
+            } else {
+                return coordinates
+            }
+        }
+    }
+
+    function start() {
+
+        /*---------- Hiển thị các file văn bản ----------*/
+        renderListFiles();
+
+        /*---------- Hiển thị list item thanh tab ----------*/
+        renderItem();
+
+        /*---------- Thêm sự kiện kéo, thả cho từng trang  ----------*/
+        handlerDrop();
+
+        /*---------- Lấy các phần tử đã được thêm vào vùng chưa văn bản, xử lí kéo thả trong vùng chứa ----------*/
+        getItemDrop();
+
+        /*---------- Xử lí tạo mới item ----------*/
+        handlerCreateItem();
+    };
+
     start();
 })();
---
+++ mock.js
@@ -0,0 +1,68 @@
+export const list = [
+    {
+        link: 'https://luatquanghuy.vn/wp-content/uploads/hop-dong-dien-tu-image-01.webp',
+        height: 700,
+        width: 500,
+    },
+    {
+        link: 'https://luatquanghuy.vn/wp-content/uploads/hop-dong-dien-tu-image-01.webp',
+        height: 700,
+        width: 500,
+    },
+    {
+        link: 'https://luatquanghuy.vn/wp-content/uploads/hop-dong-dien-tu-image-01.webp',
+        height: 700,
+        width: 500,
+    },
+    {
+        link: 'https://luatquanghuy.vn/wp-content/uploads/hop-dong-dien-tu-image-01.webp',
+        height: 700,
+        width: 500,
+    },
+];
+
+export const listInput = [
+    {
+        type: '1',
+        minWidth: 150,
+        minHeight: 80,
+        defaultWidth: 150,
+        defaultHeight: 80,
+        style:"style_1",
+        html: `
+            <div
+                draggable="true"
+                class="item style_1"
+                data-type="1"
+            >
+                Hello
+            </div>
+        `,
+        htmlInsert: `
+            <div class="editor style_1 item_main_drop" contenteditable="true" data-type="1">Hello</div>
+        `,
+    },
+    {
+        type: '2',
+        minWidth: 50,
+        minHeight: 50,
+        defaultWidth: 70,
+        defaultHeight: 70,
+        style:"style_2",
+        html: `
+        <img
+            draggable="true"
+            class="item style_2"
+            data-type="2"
+            src="https://khacdaunhanh247.com/upload/hinhanh/16072018-5716.png"
+        /> `,
+        htmlInsert: `
+            <img
+                draggable="true"
+                class="item style_2"
+                data-type="2"
+                src="https://khacdaunhanh247.com/upload/hinhanh/16072018-5716.png"
+            />
+        `,
+    },
+];
